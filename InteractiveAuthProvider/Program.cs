8d2ad22ab19bf0532d3c8880de47c58cb2738a80
Andrew Omondi <andrew.omondi@microsoft.com>
2019-12-27 12:24:07 +0300

    Working sample for interactive auth provider

--- InteractiveAuthProvider/Program.cs
+++ InteractiveAuthProvider/Program.cs
@@ -1,12 +1,43 @@
-﻿using System;
+﻿// ------------------------------------------------------------------------------
+//  Copyright (c) Microsoft Corporation.  All Rights Reserved.  Licensed under the MIT License.  See License in the project root for license information.
+// ------------------------------------------------------------------------------
+
+using Microsoft.Graph;
+using Microsoft.Identity.Client;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace InteractiveAuthProvider
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static async Task Main()
         {
-            Console.WriteLine("Hello World!");
+            string clientId = "INSERT_CLIENT_ID_HERE";
+            string[] scopes = { "User.Read" };
+
+            IPublicClientApplication publicClientApplication = PublicClientApplicationBuilder
+                .Create(clientId)
+                .WithRedirectUri("http://localhost:1234")
+                .Build();
+
+            InteractiveAuthenticationProvider authenticationProvider = new InteractiveAuthenticationProvider(publicClientApplication, scopes);
+
+            HttpClient httpClient = GraphClientFactory.Create(authenticationProvider);
+            
+            HttpRequestMessage requestMessage = new HttpRequestMessage(HttpMethod.Get, "me");
+
+            HttpResponseMessage response = await httpClient.SendAsync(requestMessage);
+            if (response.IsSuccessStatusCode)
+            {
+                string jsonResponse = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(jsonResponse);
+            }
+            else
+            {
+                Console.WriteLine("Error when making graph call");
+            }      
         }
     }
 }
